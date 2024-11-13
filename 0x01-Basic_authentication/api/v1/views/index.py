99b977bcb542fb49b47ca968ad351a47b0a1111a
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-11-13 15:35:06 +0100

    401 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort,Blueprint
 from api.v1.views import app_views
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -26,8 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'],strict_slashes=False)
-def Unauthorized() -> str:
-    """ Endpoint to test 401 error handler
-    """
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ Endpoint to test 401 error handler"""
     abort(401)
