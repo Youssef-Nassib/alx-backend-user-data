e9f39476825ad04870c5c7e78e76e1bd7bab0cef
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-11-13 14:56:52 +0100

    401 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def Unauthorized():
+@app_views.route('/unauthorized/', methods=['GET'],strict_slashes=False)
+def Unauthorized() -> str:
     """ Endpoint to test 401 error handler
     """
     abort(401)
