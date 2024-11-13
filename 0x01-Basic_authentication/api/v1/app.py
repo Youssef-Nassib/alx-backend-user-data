0d2cd11e291b4fd5e4ca790781850117fb3668d7
Youssef Nassib <wissal2015.nassib@gmail.com>
2024-11-13 16:30:33 +0100

    401

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,7 +23,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
+def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
